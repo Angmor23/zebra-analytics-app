9674e1245469889a570f7e1de9a27afdcb5e0e13
v.pirogov <v.pirogov@corp.mail.ru>
2019-10-11 19:41:53 +0300

    wip

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import Layout from '../Layout';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Layout>
-        <a
-          href="https://oauth.yandex.ru/authorize?response_type=token&client_id=c1b491bd74e84df4b313d39846f27907"
-          target="_blank"
-        >
-          Получить OAuth токен
-        </a>
-      </Layout>
-    );
-  }
-}
+const oauthUrl = 'https://oauth.yandex.ru/authorize';
+const clientId = 'c1b491bd74e84df4b313d39846f27907';
+const respType = 'token';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <Layout>
+      <input type="text" placeholder="Введите token" />
+      <button>Далее</button>
+      <a href={`${oauthUrl}?response_type=${respType}&client_id=${clientId}`} target="_blank">
+        Получить OAuth токен
+      </a>
+    </Layout>
+  );
+};
 
 export default App;
